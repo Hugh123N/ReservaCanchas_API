ea7ad99695666c5421ec5274b9f0dfa0a929b0e4
Hugh123N <hughhnp@gmail.com>
2026-05-09 12:43:37 -0500

    up

--- Reserva.Dto/Dbo/Plane/ListPlaneDto.cs
+++ Reserva.Dto/Dbo/Plane/ListPlaneDto.cs
@@ -5,6 +5,7 @@ namespace Reserva.Dto.Dbo.Plane
 {
     public class ListPlaneDto: PlaneDto
     {
+        public int IdPlane { get; set; }
         public List<PlanCaracteristicaDto>? PlanCaracteristicas { get; set; }
         public List<GetPlanTarifaDto>? PlanTarifa { get; set; }
         public List<PlanLimiteDto>? PlanLimite { get; set; }
